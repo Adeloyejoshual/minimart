5026896c5c6a191462fe3dda3b06898fd4eaf6b4
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-15 00:55:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Home from "./Home";
-import UploadProduct from "./UploadProduct";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <>
-      <UploadProduct />
-      <Home />
-    </>
-  );
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => {
+      setUser(u);
+    });
+  }, []);
+
+  return user ? <Home /> : <Login />;
 }
 
 export default App;
