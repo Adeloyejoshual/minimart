02de223887f77eb19bec8c1917b82c23d14cdaa3
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-15 00:52:17 +0100

    Create src/pages/Login.js

---
+++ src/pages/Login.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Logged in");
+    } catch (err) {
+      alert("Invalid login");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
