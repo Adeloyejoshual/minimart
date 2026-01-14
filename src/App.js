f586cb0d634f98bd321863e966fc57817781391e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-14 15:46:56 +0100

    Create src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import UploadProduct from "./UploadProduct";
+
+function App() {
+  return (
+    <>
+      <UploadProduct />
+      <Home />
+    </>
+  );
+}
+
+export default App;
