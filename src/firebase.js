d6f1a70166a2211b42353213dc4a4968407198d9
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-14 16:25:59 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBNkENjrUMZQ8Qtt6h3qa1CQPP1vXvMZ9g",
-  authDomain: "minimart-6bc70.firebaseapp.com",
-  projectId: "minimart-6bc70",
-  storageBucket: "minimart-6bc70.firebasestorage.app",
-  messagingSenderId: "454404376757",
-  appId: "1:454404376757:web:e78ed2d388e1be2cdfd6fc"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
