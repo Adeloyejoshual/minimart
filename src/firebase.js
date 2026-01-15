c1d314c6b179088443d30e5c6318271bad266f4d
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-15 01:39:42 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
+
+// src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -7,8 +10,13 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// ✅ Export auth and db
+export const auth = getAuth(app);
 export const db = getFirestore(app);
