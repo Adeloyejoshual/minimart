08225bd5d8b639e560a6fead9a0d17cb83cb04fe
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-16 20:25:03 +0100

    Create config/locations.js

---
+++ src/config/locations.js
@@ -0,0 +1,9 @@
+// /config/locations.js
+
+const locations = {
+  Lagos: ["Ikeja", "Lekki", "Yaba", "Surulere"],
+  Abuja: ["Garki", "Wuse", "Asokoro", "Maitama"],
+  Rivers: ["Port Harcourt", "Obio-Akpor", "Eleme"],
+};
+
+export default locations;
