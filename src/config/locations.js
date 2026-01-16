643a135f516854475f9264227eaa7d4fc159a43f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-17 00:33:25 +0100

    Delete src/config directory

--- src/config/locations.js
+++
@@ -1,9 +0,0 @@
-// /config/locations.js
-
-const locations = {
-  Lagos: ["Ikeja", "Lekki", "Yaba", "Surulere"],
-  Abuja: ["Garki", "Wuse", "Asokoro", "Maitama"],
-  Rivers: ["Port Harcourt", "Obio-Akpor", "Eleme"],
-};
-
-export default locations;
