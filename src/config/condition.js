7d735da69ca82e08910c242c4bb11d4443d7996e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-17 17:20:22 +0100

    Create condition.js

---
+++ src/config/condition.js
@@ -0,0 +1,15 @@
+// src/config/condition.js
+
+const conditions = {
+  main: ["New", "Refurbished", "Used"],
+  usedDetails: [
+    "Cracked body",
+    "Cracked camera",
+    "Cracked front & back",
+    "Cracked screen",
+    "No cracks",
+    "Not working battery"
+  ]
+};
+
+export default conditions;
