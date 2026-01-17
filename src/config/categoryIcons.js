9c3a19178560a70d1ea2bc65e2a699a86016f484
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-17 14:58:36 +0100

    Create config/categoryIcons.js

---
+++ src/config/categoryIcons.js
@@ -0,0 +1,17 @@
+import {
+  FaMobileAlt,
+  FaTv,
+  FaCar,
+  FaHome,
+  FaTshirt
+} from "react-icons/fa";
+
+const categoryIcons = {
+  "Mobile Phones & Tablets": FaMobileAlt,
+  Electronics: FaTv,
+  Vehicles: FaCar,
+  Property: FaHome,
+  Fashion: FaTshirt
+};
+
+export default categoryIcons;
