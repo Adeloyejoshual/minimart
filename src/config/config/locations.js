39f45bb0593e296843f21eaa359a6e7f7b834a5d
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-17 01:52:10 +0100

    Create config/locations.js

---
+++ src/config/config/locations.js
@@ -0,0 +1,13 @@
+// /config/locations.json
+{
+  "North West": {
+    "Kano": ["Ajingi","Albasu","Bagwai","Bebeji", "..."],
+    "Kaduna": ["Birnin Gwari", "..."],
+    "Katsina": ["Bakori", "..."]
+  },
+  "North East": { ... },
+  "North Central": { ... },
+  "South West": { ... },
+  "South East": { ... },
+  "South South": { ... }
+}
