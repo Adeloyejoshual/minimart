1034906c6bd583bacca9670beb22de5096cb94d8
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-17 16:37:09 +0100

    Update phoneModels.js

--- src/config/phoneModels.js
+++ src/config/phoneModels.js
@@ -1,206 +1,67 @@
 // src/config/phoneModels.js
 
 const phoneModels = {
-  AGM: ["AGM X4","AGM X3","AGM X2"],
-
-  Advan: ["Advan G3","Advan G2","Advan A8"],
-
-  Alcatel: ["Alcatel 1","Alcatel 3L","Alcatel 1B"],
-
-  Allview: ["Allview Soul X6","Allview Soul X5"],
-
-  Amoi: ["Amoi N828","Amoi A10"],
-
-  Apple: [
-    "iPhone 17 Pro Max","iPhone 17 Pro","iPhone 17 Plus","iPhone 17",
-    "iPhone 16 Pro Max","iPhone 16 Pro","iPhone 16 Plus","iPhone 16",
-    "iPhone 15 Pro Max","iPhone 15 Pro","iPhone 15 Plus","iPhone 15",
-    "iPhone 14 Pro Max","iPhone 14 Pro","iPhone 14 Plus","iPhone 14",
-    "iPhone 13 Pro Max","iPhone 13 Pro","iPhone 13 Mini","iPhone 13",
-    "iPhone 12 Pro Max","iPhone 12 Pro","iPhone 12 Mini","iPhone 12",
-    "iPhone 11 Pro Max","iPhone 11 Pro","iPhone 11",
-    "iPhone XS Max","iPhone XS","iPhone XR",
-    "iPhone X",
-    "iPhone 8 Plus","iPhone 8",
-    "iPhone 7 Plus","iPhone 7",
-    "iPhone 6s Plus","iPhone 6s",
-    "iPhone 6 Plus","iPhone 6",
-    "iPhone SE 2022","iPhone SE 2020",
-    "iPhone 5s","iPhone 5c","iPhone 5",
-    "iPhone 4s","iPhone 4","iPhone 3GS","iPhone 3G"
-  ],
-
-  Archos: ["Archos Diamond Alpha","Archos Core 60"],
-
-  Asus: [
-    "ROG Phone 8 Pro","ROG Phone 7 Ultimate","ROG Phone 7",
-    "ROG Phone 6 Pro","ROG Phone 6",
-    "Zenfone 10","Zenfone 9","Zenfone 8"
-  ],
-
-  BlackBerry: ["Key2","KeyOne","Passport","Bold","Curve"],
-
-  Blackview: ["Blackview BV9900","Blackview BV9800"],
-
-  BQ: ["BQ Aquaris X","BQ Aquaris U2"],
-
-  BLU: ["BLU G91","BLU G50"],
-
-  CAT: ["CAT S75","CAT S61"],
-
-  Casio: ["Casio G'zOne","Casio Exilim"],
-
-  "Cherry Mobile": ["Flare S7","Flare X"],
-
-  "Cherry Mobile Flare": ["Flare 6","Flare 5"],
-
-  Cubot: ["Cubot X50","Cubot King Kong 7"],
-
-  "Cubot Rugged": ["Cubot King Kong 5","Cubot Quest"],
-
-  Doogee: ["Doogee S100","Doogee N30"],
-
-  Energizer: ["Energizer Hardcase H550S","Energizer Power Max P18K"],
-
-  Elephone: ["Elephone U3","Elephone E10"],
-
-  Ericsson: ["Ericsson R380","Ericsson T28"],
-
-  Fairphone: ["Fairphone 5","Fairphone 4"],
-
-  FiiO: ["FiiO M11","FiiO M15"],
-
-  Fly: ["Fly FS511","Fly Cirrus 8"],
-
-  Gionee: ["Gionee M12","Gionee P7"],
-
-  Gigabyte: ["Gigabyte GSmart Saga","Gigabyte GSmart Aku"],
-
-  Google: ["Pixel 8 Pro","Pixel 8","Pixel 7 Pro","Pixel 7"],
-
-  Gtel: ["Gtel A1","Gtel S9"],
-
-  Homtom: ["Homtom HT70","Homtom HT50"],
-
-  HTC: ["U12 Plus","U11 Plus","U11","One M9","One M8"],
-
-  Huawei: [
-    "P60 Pro","P50 Pro",
-    "P40 Pro Plus","P40 Pro","P40",
-    "P30 Pro","P30",
-    "Mate 50 Pro","Mate 40 Pro","Mate 30 Pro","Mate 20 Pro"
-  ],
-
-  Honor: ["Honor Magic 6","Honor 90","Honor X8a"],
-
-  "i-mobile": ["i-mobile IQ II","i-mobile iQ X"],
-
-  itel: ["S23 Plus","S23","S21","A58 Pro","A56","A48","A25","A16"],
-
-  "Itel Mobile": ["Itel S16","Itel A58"],
-
-  Jolla: ["Jolla Phone"],
-
-  Karbonn: ["Karbonn Titanium S9","Karbonn Aura Note"],
-
-  "Karbonn Titanium": ["Karbonn Titanium S6","Karbonn Titanium S5"],
-
-  Kyocera: ["Kyocera DuraForce Pro 3","Kyocera Rise"],
-
-  Lava: ["Lava Z6","Lava Agni 2"],
-
-  "Lava Iris": ["Lava Iris 88","Lava Iris 86"],
-
-  Lenovo: ["Lenovo Legion Y90","Lenovo K14 Note"],
-
-  LeEco: ["LeEco Le Max 3","LeEco Le S3"],
-
-  Maze: ["Maze Alpha","Maze Blade"],
-
-  Maxcom: ["Maxcom MM720"],
-
-  Meizu: ["Meizu 20","Meizu 19"],
-
-  Micromax: ["Micromax In Note 1","Micromax In 1B"],
-
-  "Micromax Canvas": ["Micromax Canvas 2","Micromax Canvas 3"],
-
-  Motorola: ["Edge 40 Pro","Edge 40","G73","G54","G9 Plus","G8 Plus"],
-
-  MyPhone: ["MyPhone My72","MyPhone My11"],
-
-  NEC: ["NEC Terrain"],
-
-  Nokia: ["X30","X20","G21","G20","C31","C21","3310","105","6300"],
-
-  Nomu: ["Nomu S30","Nomu S20"],
-
-  Nothing: ["Nothing Phone 2","Nothing Phone 1"],
-
-  Nubia: ["Nubia Red Magic 8","Nubia Z50"],
-
-  Oppo: [
-    "Find X6 Pro","Find X5 Pro",
-    "Reno 10 Pro Plus","Reno 10 Pro","Reno 10",
-    "Reno 9 Pro","Reno 9",
-    "A98","A77","A57","A54"
-  ],
-
-  "Oppo Real": ["Realme X3","Realme X50"],
-
-  Onyx: ["Onyx Boox Note","Onyx Boox Nova"],
-
-  Panasonic: ["Panasonic Eluga X1","Panasonic Eluga Ray"],
-
-  Poco: ["Poco X5 Pro","Poco X5","Poco X4 Pro","Poco X3 Pro"],
-
-  "Posh Mobile": ["Posh Mobile R100","Posh Mobile R120"],
-
-  Prestigio: ["Prestigio Muze A9","Prestigio Grace"],
-
-  QMobile: ["QMobile LT500","QMobile QInfinity"],
-
-  RCA: ["RCA Cambio Plus","RCA Voyager"],
-
-  Realme: ["GT 5 Pro","GT 3","GT 2 Pro","Narzo 60 Pro","Narzo 60","C55","C35"],
-
-  Saygus: ["Saygus VPhone V1","Saygus VPhone V2"],
-
-  Sharp: ["Sharp Aquos R7","Sharp Aquos Sense 7"],
-
-  Siemens: ["Siemens Xelibri 6","Siemens S65"],
-
-  Sony: ["Xperia 1 V","Xperia 1 IV","Xperia 5 IV","Xperia 5 III","Xperia XZ Premium","Xperia Z5 Premium"],
-
-  Spice: ["Spice Stellar 518","Spice Xlife"],
-
-  TCL: ["TCL 30 SE","TCL 20 Pro"],
-
-  Tecno: ["Camon 20 Premier","Camon 20 Pro 5G","Camon 20 Pro","Camon 20","Camon 19 Pro","Camon 19","Camon 18 Premier","Camon 18 Pro","Camon 18","Spark 10 Pro","Spark 10","Spark 9 Pro","Spark 9","Pouvoir 4 Pro","Pouvoir 4"],
-
-  "Tecno Mobile": ["Tecno Spark 10","Tecno Camon 19"],
-
-  Toshiba: ["Toshiba TG01","Toshiba Portege"],
-
-  Ulefone: ["Ulefone Armor 17","Ulefone Note 13"],
-
-  Unihertz: ["Unihertz Jelly 3","Unihertz Titan"],
-
-  "VK Mobile": ["VK Mobile VK7","VK Mobile VK8"],
-
-  Vivo: ["X100 Pro","X100","X90 Pro","X90","V29 Pro","V27 Pro","V27","Y36","Y22","Y21","Y20"],
-
-  Vkworld: ["Vkworld VK7000","Vkworld VK8000"],
-
-  Wiko: ["Wiko Power U30","Wiko Y81"],
-
-  Xiaomi: ["Redmi Note 13 Pro Plus","Redmi Note 13 Pro","Redmi Note 13","Redmi Note 12 Pro Plus","Redmi Note 12 Pro","Redmi Note 12","Redmi Note 11 Pro Plus","Redmi Note 11 Pro","Redmi Note 11","Mi 13 Pro","Mi 13","Mi 12 Pro","Mi 12","Mi 11 Pro","Mi 11","Poco X5 Pro","Poco X5","Poco X4 Pro","Poco X3 Pro"],
-
-  Zonda: ["Zonda Z1","Zonda Z2"],
-
-  "Zonda Mobile": ["Zonda Mobile M1","Zonda Mobile M2"],
-
-  ZTE: ["ZTE Axon 50","ZTE Blade 21"]
+  Smartphones: {
+    AGM: ["AGM X4","AGM X3","AGM X2"],
+    Advan: ["Advan G3","Advan G2","Advan A8"],
+    Alcatel: ["Alcatel 1","Alcatel 3L","Alcatel 1B"],
+    Allview: ["Allview Soul X6","Allview Soul X5"],
+    Amoi: ["Amoi N828","Amoi A10"],
+    Apple: [
+      "iPhone 17 Pro Max","iPhone 17 Pro","iPhone 17 Plus","iPhone 17",
+      "iPhone 16 Pro Max","iPhone 16 Pro","iPhone 16 Plus","iPhone 16",
+      "iPhone 15 Pro Max","iPhone 15 Pro","iPhone 15 Plus","iPhone 15",
+      "iPhone 14 Pro Max","iPhone 14 Pro","iPhone 14 Plus","iPhone 14",
+      "iPhone 13 Pro Max","iPhone 13 Pro","iPhone 13 Mini","iPhone 13",
+      "iPhone 12 Pro Max","iPhone 12 Pro","iPhone 12 Mini","iPhone 12",
+      "iPhone 11 Pro Max","iPhone 11 Pro","iPhone 11",
+      "iPhone XS Max","iPhone XS","iPhone XR",
+      "iPhone X",
+      "iPhone 8 Plus","iPhone 8",
+      "iPhone 7 Plus","iPhone 7",
+      "iPhone 6s Plus","iPhone 6s",
+      "iPhone 6 Plus","iPhone 6",
+      "iPhone SE 2022","iPhone SE 2020",
+      "iPhone 5s","iPhone 5c","iPhone 5",
+      "iPhone 4s","iPhone 4","iPhone 3GS","iPhone 3G"
+    ],
+    Asus: ["ROG Phone 8 Pro","ROG Phone 7 Ultimate","ROG Phone 7","ROG Phone 6 Pro","ROG Phone 6","Zenfone 10","Zenfone 9","Zenfone 8"],
+    BlackBerry: ["Key2","KeyOne","Passport","Bold","Curve"],
+    Blackview: ["Blackview BV9900","Blackview BV9800"],
+    BQ: ["BQ Aquaris X","BQ Aquaris U2"],
+    BLU: ["BLU G91","BLU G50"],
+    CAT: ["CAT S75","CAT S61"],
+    Casio: ["Casio G'zOne","Casio Exilim"],
+    "Cherry Mobile": ["Flare S7","Flare X"],
+    Cubot: ["Cubot X50","Cubot King Kong 7"],
+    Doogee: ["Doogee S100","Doogee N30"],
+    Energizer: ["Energizer Hardcase H550S","Energizer Power Max P18K"],
+    Elephone: ["Elephone U3","Elephone E10"],
+    Fairphone: ["Fairphone 5","Fairphone 4"],
+    Fly: ["Fly FS511","Fly Cirrus 8"],
+    Gionee: ["Gionee M12","Gionee P7"],
+    Google: ["Pixel 8 Pro","Pixel 8","Pixel 7 Pro","Pixel 7"],
+    Huawei: ["P60 Pro","P50 Pro","P40 Pro Plus","P40 Pro","P40","P30 Pro","P30","Mate 50 Pro","Mate 40 Pro","Mate 30 Pro","Mate 20 Pro"],
+    Honor: ["Honor Magic 6","Honor 90","Honor X8a"],
+    itel: ["S23 Plus","S23","S21","A58 Pro","A56","A48","A25","A16"],
+    Jolla: ["Jolla Phone"],
+    Karbonn: ["Karbonn Titanium S9","Karbonn Aura Note"],
+    Lenovo: ["Lenovo Legion Y90","Lenovo K14 Note"],
+    Motorola: ["Edge 40 Pro","Edge 40","G73","G54","G9 Plus","G8 Plus"],
+    Nokia: ["X30","X20","G21","G20","C31","C21","3310","105","6300"],
+    Oppo: ["Find X6 Pro","Find X5 Pro","Reno 10 Pro Plus","Reno 10 Pro","Reno 10","Reno 9 Pro","Reno 9","A98","A77","A57","A54"],
+    Realme: ["GT 5 Pro","GT 3","GT 2 Pro","Narzo 60 Pro","Narzo 60","C55","C35"],
+    Samsung: ["Galaxy S23","Galaxy S22","Galaxy S21","Galaxy A54","Galaxy A14"], // Example
+    Tecno: ["Camon 20 Premier","Camon 20 Pro 5G","Camon 20 Pro","Camon 20","Camon 19 Pro","Camon 19","Camon 18 Premier","Camon 18 Pro","Camon 18","Spark 10 Pro","Spark 10","Spark 9 Pro","Spark 9","Pouvoir 4 Pro","Pouvoir 4"],
+    Vivo: ["X100 Pro","X100","X90 Pro","X90","V29 Pro","V27 Pro","V27","Y36","Y22","Y21","Y20"],
+    Xiaomi: ["Redmi Note 13 Pro Plus","Redmi Note 13 Pro","Redmi Note 13","Redmi Note 12 Pro Plus","Redmi Note 12 Pro","Redmi Note 12","Redmi Note 11 Pro Plus","Redmi Note 11 Pro","Redmi Note 11","Mi 13 Pro","Mi 13","Mi 12 Pro","Mi 12","Mi 11 Pro","Mi 11"],
+  },
+  Tablets: {
+    Apple: ["iPad Air","iPad Mini","iPad Pro 12.9","iPad Pro 11"],
+    Samsung: ["Galaxy Tab S8","Galaxy Tab A8"],
+    Lenovo: ["Lenovo Tab M10","Lenovo Tab P11"],
+    Huawei: ["Huawei MatePad 11","Huawei MatePad T10"],
+  }
 };
 
 export default phoneModels;
