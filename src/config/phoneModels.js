5adef3599ebe8749958b5961597464912ea96b63
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-18 01:09:15 +0100

    Update phoneModels.js

--- src/config/phoneModels.js
+++ src/config/phoneModels.js
@@ -1,5 +1,3 @@
-// src/config/phoneModels.js
-
 const phoneModels = {
   Smartphones: {
     AGM: ["AGM X4", "AGM X3", "AGM X2"],
@@ -7,6 +5,7 @@ const phoneModels = {
     Alcatel: ["Alcatel 1", "Alcatel 3L", "Alcatel 1B"],
     Allview: ["Allview Soul X6", "Allview Soul X5"],
     Amoi: ["Amoi N828", "Amoi A10"],
+
     Apple: [
       "iPhone 17 Pro Max","iPhone 17 Pro","iPhone 17 Plus","iPhone 17",
       "iPhone 16 Pro Max","iPhone 16 Pro","iPhone 16 Plus","iPhone 16",
@@ -25,72 +24,58 @@ const phoneModels = {
       "iPhone 5s","iPhone 5c","iPhone 5",
       "iPhone 4s","iPhone 4","iPhone 3GS","iPhone 3G"
     ],
+
     Asus: ["ROG Phone 8 Pro","ROG Phone 7 Ultimate","ROG Phone 7","ROG Phone 6 Pro","ROG Phone 6","Zenfone 10","Zenfone 9","Zenfone 8"],
     BlackBerry: ["Key2","KeyOne","Passport","Bold","Curve"],
-    Blackview: ["Blackview BV9900","Blackview BV9800"],
-    BQ: ["BQ Aquaris X","BQ Aquaris U2"],
-    BLU: ["BLU G91","BLU G50"],
-    CAT: ["CAT S75","CAT S61"],
-    Casio: ["Casio G'zOne","Casio Exilim"],
-    "Cherry Mobile": ["Flare S7","Flare X"],
-    Cubot: ["Cubot X50","Cubot King Kong 7"],
-    Doogee: ["Doogee S100","Doogee N30"],
-    Energizer: ["Energizer Hardcase H550S","Energizer Power Max P18K"],
-    Elephone: ["Elephone U3","Elephone E10"],
-    Fairphone: ["Fairphone 5","Fairphone 4"],
-    Fly: ["Fly FS511","Fly Cirrus 8"],
-    Gionee: ["Gionee M12","Gionee P7"],
+    Blackview: ["BV9900","BV9800"],
+    BLU: ["G91","G50"],
+    CAT: ["S75","S61"],
+    Casio: ["G'zOne","Exilim"],
+    Cubot: ["X50","King Kong 7"],
+    Doogee: ["S100","N30"],
+    Fairphone: ["5","4"],
+    Gionee: ["M12","P7"],
     Google: ["Pixel 8 Pro","Pixel 8","Pixel 7 Pro","Pixel 7"],
-    Huawei: ["P60 Pro","P50 Pro","P40 Pro Plus","P40 Pro","P40","P30 Pro","P30","Mate 50 Pro","Mate 40 Pro","Mate 30 Pro","Mate 20 Pro"],
-    Honor: ["Honor Magic 6","Honor 90","Honor X8a"],
-    itel: ["S23 Plus","S23","S21","A58 Pro","A56","A48","A25","A16"],
-    Jolla: ["Jolla Phone"],
-    Karbonn: ["Karbonn Titanium S9","Karbonn Aura Note"],
-    Lenovo: ["Lenovo Legion Y90","Lenovo K14 Note"],
-    Motorola: ["Edge 40 Pro","Edge 40","G73","G54","G9 Plus","G8 Plus"],
-    Nokia: ["X30","X20","G21","G20","C31","C21","3310","105","6300"],
-    Oppo: ["Find X6 Pro","Find X5 Pro","Reno 10 Pro Plus","Reno 10 Pro","Reno 10","Reno 9 Pro","Reno 9","A98","A77","A57","A54"],
-    Realme: ["GT 5 Pro","GT 3","GT 2 Pro","Narzo 60 Pro","Narzo 60","C55","C35"],
+    Huawei: ["P60 Pro","P50 Pro","P40 Pro","P30 Pro","Mate 50 Pro","Mate 40 Pro"],
+    Honor: ["Magic 6","90","X8a"],
+    Itel: ["S23 Plus","S23","S21","A58 Pro","A56","A48","A25","A16"],
+    Lenovo: ["Legion Y90","K14 Note"],
+    Motorola: ["Edge 40 Pro","Edge 40","G73","G54"],
+    Nokia: ["X30","G21","C31","3310","105"],
+    Oppo: ["Find X6 Pro","Reno 10 Pro","A98","A77","A57"],
+    Realme: ["GT 5 Pro","Narzo 60 Pro","C55"],
     Samsung: [
       "Galaxy S23","Galaxy S23+","Galaxy S23 Ultra",
       "Galaxy S22","Galaxy S22+","Galaxy S22 Ultra",
-      "Galaxy S21","Galaxy S21+","Galaxy S21 Ultra",
-      "Galaxy S20","Galaxy S20+","Galaxy S20 Ultra",
-      "Galaxy A14","Galaxy A14 5G","Galaxy A24","Galaxy A34","Galaxy A54","Galaxy A54 5G","Galaxy A74",
-      "Galaxy Note 20","Galaxy Note 20 Ultra","Galaxy Note 10","Galaxy Note 10+",
-      "Galaxy Z Fold 5","Galaxy Z Fold 4","Galaxy Z Flip 5","Galaxy Z Flip 4",
-      "Galaxy M14","Galaxy M13","Galaxy M33","Galaxy M34"
+      "Galaxy A14","Galaxy A24","Galaxy A34","Galaxy A54",
+      "Galaxy Z Fold 5","Galaxy Z Flip 5"
     ],
-    Tecno: ["Camon 20 Premier","Camon 20 Pro 5G","Camon 20 Pro","Camon 20","Camon 19 Pro","Camon 19","Camon 18 Premier","Camon 18 Pro","Camon 18","Spark 10 Pro","Spark 10","Spark 9 Pro","Spark 9","Pouvoir 4 Pro","Pouvoir 4"],
-    Vivo: ["X100 Pro","X100","X90 Pro","X90","V29 Pro","V27 Pro","V27","Y36","Y22","Y21","Y20"],
-    Xiaomi: ["Redmi Note 13 Pro Plus","Redmi Note 13 Pro","Redmi Note 13","Redmi Note 12 Pro Plus","Redmi Note 12 Pro","Redmi Note 12","Redmi Note 11 Pro Plus","Redmi Note 11 Pro","Redmi Note 11","Mi 13 Pro","Mi 13","Mi 12 Pro","Mi 12","Mi 11 Pro","Mi 11"],
+    Tecno: ["Camon 20 Premier","Spark 10 Pro","Spark 9"],
+    Vivo: ["X100 Pro","X90","V29 Pro","Y36"],
+    Xiaomi: ["Redmi Note 13 Pro","Redmi Note 12","Mi 13","Mi 12"],
 
-    Other: [
-      "AGM","Alba","Alcatel","AllCall","Allview","Amoi","Asus","BlackBerry","Blackview","BLU",
-      "Samsung","Apple","Xiaomi","Tecno","Infinix","Huawei","Oppo","Realme","Vivo","Nokia",
-      "Lenovo","Motorola","Honor","Itel","Casio","Cubot","Doogee","Fairphone","Fly","Gionee","Google"
-    ]
+    Other: ["Other Brand"]
+  },
+
+  "Feature Phones": {
+    Nokia: ["105","106","110","150","3310","6300"],
+    Itel: ["A16","A25","A48","A56","A58 Pro"],
+    Samsung: ["Guru Music 2","E1200","E1270"],
+    Tecno: ["T401","T702"],
+    Other: ["Other Feature Phone"]
   },
 
   Tablets: {
-    Apple: ["iPad Air","iPad Mini","iPad Pro 12.9","iPad Pro 11"],
-    Samsung: ["Galaxy Tab S8","Galaxy Tab S8+","Galaxy Tab S7","Galaxy Tab A8"],
-    Lenovo: ["Lenovo Tab M10","Lenovo Tab P11"],
-    Huawei: ["Huawei MatePad 11","Huawei MatePad T10"]
-  },
-
-  FeaturePhones: {
-    Nokia: ["105","106","110","150","3310","6300"],
-    Itel: ["A16","A25","A48","A56","A58 Pro","S21","S23","S23 Plus"],
-    Samsung: ["Guru Music 2","E1200","E1270","E1230"],
-    Tecno: ["T401","T401i","T702"],
-    Other: ["Alcatel","Karbonn","Infinix","Gionee","LG"]
+    Apple: ["iPad Pro","iPad Air","iPad Mini"],
+    Samsung: ["Galaxy Tab S8","Galaxy Tab A8"],
+    Lenovo: ["Tab M10","Tab P11"],
+    Huawei: ["MatePad 11","MatePad T10"]
   },
 
   Accessories: {
     Chargers: ["Samsung Charger","Apple Charger","Tecno Charger","Itel Charger"],
-    Headphones: ["Sony Headphones","Samsung Headphones","Apple AirPods","Beats"],
-    Cases: ["Samsung Case","Apple Case","Xiaomi Case","Tecno Case"],
+    Headphones: ["Sony","Samsung","AirPods","Beats"],
+    Cases: ["Samsung Case","Apple Case","Xiaomi Case"],
     Other: ["Other Accessories"]
   }
 };
