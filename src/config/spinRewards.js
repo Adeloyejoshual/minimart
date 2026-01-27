66b30d0615a2e38212c2beb001a78f67d7cc3cb8
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-01-27 09:56:41 +0100

    Update spinRewards.js

--- src/config/spinRewards.js
+++ src/config/spinRewards.js
@@ -1,12 +1,54 @@
 // src/config/spinRewards.js
-import { FaGift } from "react-icons/fa";
+import { FaGift, FaTruck, FaPercentage, FaCoins } from "react-icons/fa";
 
 export const spinRewards = [
-  { id: 1, label: "₦500 Off", type: "money", value: 500, icon: <FaGift /> },
-  { id: 2, label: "Free Delivery", type: "freeDelivery", value: 2, icon: <FaGift /> },
-  { id: 3, label: "₦1000 Off", type: "money", value: 1000, icon: <FaGift /> },
-  { id: 4, label: "10% Off", type: "percentage", value: 10, icon: <FaGift /> },
-  { id: 5, label: "Free Shipping for 2 shipped", type: "freeDelivery", value: 2, icon: <FaGift /> },
-  { id: 6, label: "No Reward", type: "none", value: 0, icon: <FaGift /> },
-  { id: 7, label: "Spin Again", type: "repeat", value: 0, icon: <FaGift /> },
+  {
+    id: 1,
+    label: "₦500 Off",
+    type: "money",
+    value: 500,
+    icon: <FaCoins />,
+  },
+  {
+    id: 2,
+    label: "Free Delivery",
+    type: "freeDelivery",
+    value: 1, // number of free deliveries
+    icon: <FaTruck />,
+  },
+  {
+    id: 3,
+    label: "₦1000 Off",
+    type: "money",
+    value: 1000,
+    icon: <FaCoins />,
+  },
+  {
+    id: 4,
+    label: "10% Off",
+    type: "percentage",
+    value: 10,
+    icon: <FaPercentage />,
+  },
+  {
+    id: 5,
+    label: "Free Shipping for 2 Shipped",
+    type: "freeDelivery",
+    value: 2,
+    icon: <FaTruck />,
+  },
+  {
+    id: 6,
+    label: "Spin Again",
+    type: "repeat",
+    value: 0,
+    icon: <FaGift />,
+  },
+  {
+    id: 7,
+    label: "No Reward",
+    type: "none",
+    value: 0,
+    icon: <FaGift />,
+  },
 ];
