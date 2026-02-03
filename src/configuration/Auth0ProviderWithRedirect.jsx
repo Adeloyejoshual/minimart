0befec72dd34296ab03aeb984745c7ef33492646
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-03 09:02:52 +0100

    Create src/configuration/Auth0ProviderWithRedirect.jsx

---
+++ src/configuration/Auth0ProviderWithRedirect.jsx
@@ -0,0 +1,17 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const Auth0ProviderWithRedirect = ({ children }) => {
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithRedirect;
