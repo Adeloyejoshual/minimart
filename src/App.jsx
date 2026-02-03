11ecf7bd9ef4a9cb46398b938addeeb5a109b247
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-03 09:59:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 import HomePage from "./pages/HomePage.jsx";
 
-// -------------------- MiniMart Pages --------------------
+// MiniMart
 import ProductDetail from "./pages/minimart/ProductDetail.jsx";
 import MiniMartCartPage from "./pages/minimart/MiniMartCartPage.jsx";
 import MiniMartCheckoutPage from "./pages/minimart/MiniMartCheckoutPage.jsx";
 import MiniMartOrderTrackingPage from "./pages/minimart/MiniMartOrderTrackingPage.jsx";
 
-// -------------------- Marketplace Pages --------------------
+// Marketplace
 import MarketplaceListingDetailsPage from "./pages/marketplace/MarketplaceListingDetailsPage.jsx";
 import MarketplaceAddProductPage from "./pages/marketplace/MarketplaceAddProductPage.jsx";
 import MarketplaceChatPage from "./pages/marketplace/MarketplaceChatPage.jsx";
+
+// Auth
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function App() {
   return (
@@ -19,16 +26,29 @@ function App() {
         {/* Home */}
         <Route path="/" element={<HomePage />} />
 
-        {/* MiniMart */}
+        {/* Auth */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* MiniMart - some protected */}
         <Route path="/minimart/product/:id" element={<ProductDetail />} />
-        <Route path="/minimart/cart" element={<MiniMartCartPage />} />
-        <Route path="/minimart/checkout" element={<MiniMartCheckoutPage />} />
-        <Route path="/minimart/order/:id" element={<MiniMartOrderTrackingPage />} />
+        <Route
+          path="/minimart/cart"
+          element={<PrivateRoute><MiniMartCartPage /></PrivateRoute>}
+        />
+        <Route
+          path="/minimart/checkout"
+          element={<PrivateRoute><MiniMartCheckoutPage /></PrivateRoute>}
+        />
+        <Route
+          path="/minimart/order/:id"
+          element={<PrivateRoute><MiniMartOrderTrackingPage /></PrivateRoute>}
+        />
 
         {/* Marketplace */}
         <Route path="/marketplace/listing/:id" element={<MarketplaceListingDetailsPage />} />
-        <Route path="/marketplace/add-product" element={<MarketplaceAddProductPage />} />
-        <Route path="/marketplace/chat" element={<MarketplaceChatPage />} />
+        <Route path="/marketplace/add-product" element={<PrivateRoute><MarketplaceAddProductPage /></PrivateRoute>} />
+        <Route path="/marketplace/chat" element={<PrivateRoute><MarketplaceChatPage /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   );
