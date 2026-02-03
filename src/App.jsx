eed1af2e53187f479b4aa931dc75b6471287fe76
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-03 22:32:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
-
-// MiniMart
-import ProductDetail from "./pages/minimart/ProductDetail.jsx";
-import MiniMartCartPage from "./pages/minimart/MiniMartCartPage.jsx";
-import MiniMartCheckoutPage from "./pages/minimart/MiniMartCheckoutPage.jsx";
-import MiniMartOrderTrackingPage from "./pages/minimart/MiniMartOrderTrackingPage.jsx";
-
-// Marketplace
-import MarketplaceListingDetailsPage from "./pages/marketplace/MarketplaceListingDetailsPage.jsx";
-import MarketplaceAddProductPage from "./pages/marketplace/MarketplaceAddProductPage.jsx";
-import MarketplaceChatPage from "./pages/marketplace/MarketplaceChatPage.jsx";
-
-// Auth
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Home */}
-        <Route path="/" element={<HomePage />} />
-
-        {/* Auth */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* MiniMart - some protected */}
-        <Route path="/minimart/product/:id" element={<ProductDetail />} />
-        <Route
-          path="/minimart/cart"
-          element={<PrivateRoute><MiniMartCartPage /></PrivateRoute>}
-        />
-        <Route
-          path="/minimart/checkout"
-          element={<PrivateRoute><MiniMartCheckoutPage /></PrivateRoute>}
-        />
-        <Route
-          path="/minimart/order/:id"
-          element={<PrivateRoute><MiniMartOrderTrackingPage /></PrivateRoute>}
-        />
-
-        {/* Marketplace */}
-        <Route path="/marketplace/listing/:id" element={<MarketplaceListingDetailsPage />} />
-        <Route path="/marketplace/add-product" element={<PrivateRoute><MarketplaceAddProductPage /></PrivateRoute>} />
-        <Route path="/marketplace/chat" element={<PrivateRoute><MarketplaceChatPage /></PrivateRoute>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
