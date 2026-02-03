30e5553529fbce11231051fe880886768983bbd3
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-03 08:52:14 +0100

    Delete src directory

--- src/config/referral.js
+++
@@ -1,9 +0,0 @@
-// src/config/referral.js
-import { FaGift } from "react-icons/fa";
-
-export const referralConfig = {
-  bonusAmount: 500,  // ₦500 per referral
-  maxReferralsPerUser: 10,
-  icon: <FaGift />,
-  message: "Invite friends and earn ₦500 per successful referral!",
-};
--- src/services/uploadService.js
+++
@@ -1,8 +0,0 @@
-// /services/uploadService.js
-
-import { uploadToCloudinary } from "../cloudinary";
-
-export const uploadImages = async (files) => {
-  const uploads = files.map(file => uploadToCloudinary(file));
-  return Promise.all(uploads);
-};
