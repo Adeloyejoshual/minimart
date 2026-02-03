df9157724656c440ab69d26f3dc2924c40e69327
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-03 22:56:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+// vite.config.js
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "VITE_");
-
-  return defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
-    },
-    server: {
-      port: env.VITE_DEV_PORT ? parseInt(env.VITE_DEV_PORT) : 5173,
-      open: true,
-      cors: true,
-    },
-    build: {
-      outDir: "build",
-      sourcemap: true,
-    },
-    define: {
-      "process.env": env,
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "build", // matches your Express code
+  },
+});
