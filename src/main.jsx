4db2c622783439875c3257043914a2040ac50434
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-04 00:29:48 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
-
-// Get environment variables from .env
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+import { BrowserRouter } from "react-router-dom";
+import Auth0ProviderWithRedirect from "./configuration/Auth0ProviderWithRedirect.jsx";
+import "./styles/globalStyles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
-    >
+  <BrowserRouter>
+    <Auth0ProviderWithRedirect>
       <App />
-    </Auth0Provider>
-  </React.StrictMode>
+    </Auth0ProviderWithRedirect>
+  </BrowserRouter>
 );
