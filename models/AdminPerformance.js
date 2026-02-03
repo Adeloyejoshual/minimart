9a889cd70617d24e7760b7edd101ed998ce4d41e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-03 08:51:31 +0100

    Delete models directory

--- models/AdminPerformance.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const performanceSchema = new mongoose.Schema({
-  adminEmail: String,
-  count: { type: Number, default: 0 }, // disputes resolved
-});
-
-export default mongoose.model("AdminPerformance", performanceSchema);
--- models/MartProduct.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const MartProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  createdAt: Date,
-});
-
-module.exports = mongoose.model("MartProduct", MartProductSchema);
