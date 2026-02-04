b627ec6b7e488e18b281168cea9d4aa056e3c474
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-04 23:27:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React from "react";  
-import { Routes, Route } from "react-router-dom";  
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-// Pages  
-import HomePage from "./pages/HomePage.jsx";  
-import LoginPage from "./pages/LoginPage.jsx";  
-import RegisterPage from "./pages/RegisterPage.jsx";  
-import AddProductPage from "./pages/marketplace/AddProduct.jsx"; // <-- added
+// Pages
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 
-function App() {  
-  return (  
-    <Routes>  
-      <Route path="/" element={<HomePage />} />  
-      <Route path="/login" element={<LoginPage />} />  
-      <Route path="/register" element={<RegisterPage />} />  
-      <Route path="/marketplace/add-product" element={<AddProductPage />} /> {/* new route */}
-    </Routes>  
-  );  
-}  
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
+}
 
 export default App;
