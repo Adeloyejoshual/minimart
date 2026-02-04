495189f96913e2a21d4092b009d8992f83b28c32
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-05 00:35:39 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,28 @@
-// src/main.jsx
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const Auth0ProviderWithRedirect = ({ children }) => {
+  const navigate = useNavigate();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || "/");
+  };
+
+  return (
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: window.location.origin }}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+      }}
+      cacheLocation="localstorage" 
+      useRefreshTokens={true}
+      onRedirectCallback={onRedirectCallback}
     >
-      <App />
+      {children}
     </Auth0Provider>
-  </React.StrictMode>
-);
+  );
+};
+
+export default Auth0ProviderWithRedirect;
