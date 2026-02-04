61baa29868d6252739d2834c436db1780e37ba3e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-04 15:38:08 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
-import "../../styles/homepage.css";
+import "../styles/homepage.css";
 
 // Use .env variable
 const BACKEND_URL = import.meta.env.VITE_API_BASE_URL;
