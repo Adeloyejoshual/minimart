1f9bfab5e426e67cdd22ccf8748ba6daa6879841
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-04 01:14:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
 
 // Pages
 import HomePage from "./pages/HomePage.jsx";
@@ -8,10 +7,6 @@ import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 
 function App() {
-  const { isLoading } = useAuth0();
-
-  if (isLoading) return <div>Loading...</div>; // Show loading while Auth0 checks session
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
