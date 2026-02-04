cd97687bccef8cfb96f3d5f0157532b5ba18f8a3
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-04 01:15:09 +0100

    Update Auth0ProviderWithRedirect.jsx

--- src/configuration/Auth0ProviderWithRedirect.jsx
+++ src/configuration/Auth0ProviderWithRedirect.jsx
@@ -5,19 +5,19 @@ import { useNavigate } from "react-router-dom";
 const Auth0ProviderWithRedirect = ({ children }) => {
   const navigate = useNavigate();
 
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
-
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || "/");
   };
 
   return (
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+      }}
+      cacheLocation="localstorage" 
+      useRefreshTokens={true}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
