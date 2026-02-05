fe3fa8e241e197718fc8aa20b65a16cd0be870e5
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-05 12:35:14 +0100

    Delete src/components directory

--- src/components/layout/FooterSection.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/layout/FooterSection.jsx
-export default function FooterSection() {
-  return (
-    <footer className="bg-gray-800 text-white p-6 text-center">
-      <p>© 2026 MiniMart Marketplace. All rights reserved.</p>
-    </footer>
-  );
-}
