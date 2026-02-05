f767949de89bf19ff35ffe285e19e2444a093a65
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-05 01:16:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import TestAuth from "./pages/TestAuth.jsx";
 
-// Pages
-import HomePage from "./pages/HomePage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import RegisterPage from "./pages/RegisterPage.jsx";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
-  );
-}
-
-export default App;
+<Route path="/test-auth" element={<TestAuth />} />
