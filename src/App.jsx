18d200223adcaaf721556034da94bd512c209cfe
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-05 12:45:05 +0100

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>MiniMart is Live 🚀</h1>
+      <p>If you can see this, React is working on Render.</p>
+    </div>
+  );
+}
