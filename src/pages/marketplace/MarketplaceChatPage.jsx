664bfb5bf8e630473b8192bd24f3422bca7d4e37
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 00:58:36 +0100

    Create src/pages/marketplace/MarketplaceChatPage.jsx

---
+++ src/pages/marketplace/MarketplaceChatPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MarketplaceChatPage() {
+  return (
+    <div>
+      <h1>Marketplace Chat</h1>
+      <p>Chat with other users here.</p>
+    </div>
+  );
+}
