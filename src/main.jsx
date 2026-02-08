4be0b370c3e45d501850239415d09a5cd5a7d776
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 00:41:42 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Auth0ProviderWithRedirect from "./configuration/Auth0ProviderWithRedirect";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import Auth0ProviderWithRedirect from "./configuration/Auth0ProviderWithRedirect.jsx";
+import SyncUser from "./components/SyncUser.jsx";
+import "./styles/globalStyles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0ProviderWithRedirect>
-    <App />
-  </Auth0ProviderWithRedirect>
+  <BrowserRouter>
+    <Auth0ProviderWithRedirect>
+      <SyncUser />
+      <App />
+    </Auth0ProviderWithRedirect>
+  </BrowserRouter>
 );
