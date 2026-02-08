fc2550452273fe6a34e41882e3645569ddc9616c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-08 21:12:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
 
 function Home() {
-  return <h2>Home Page</h2>;
-}
-
-function Login() {
-  return <h2>Login Page</h2>;
+  return <h2>Home</h2>;
 }
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav style={{ display: "flex", gap: "20px" }}>
+      <nav style={{ display: "flex", gap: "20px", padding: "20px" }}>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
