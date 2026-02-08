bd8ae358de538ca4e1bf9d7d10a5e37230522315
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-08 21:15:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Auth0ProviderWithRedirect from "./configuration/Auth0ProviderWithRedirect";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Auth0ProviderWithRedirect>
     <App />
-  </React.StrictMode>
+  </Auth0ProviderWithRedirect>
 );
