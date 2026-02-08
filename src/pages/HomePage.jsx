fbd47431a3ffb5759ce3eb47dee733e7e130c2ee
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 00:55:27 +0100

    Create src/pages/HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>MiniMart - Home</h1>
+      <p>Welcome! Use the navigation to explore the app.</p>
+    </div>
+  );
+}
