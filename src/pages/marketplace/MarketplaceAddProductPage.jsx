a38ad0e08d502e3d81192328a71c7ed17d5b9d18
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 00:58:15 +0100

    Create src/pages/marketplace/MarketplaceAddProductPage.jsx

---
+++ src/pages/marketplace/MarketplaceAddProductPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MarketplaceAddProductPage() {
+  return (
+    <div>
+      <h1>Add Product</h1>
+      <p>Form to add a new marketplace product.</p>
+    </div>
+  );
+}
