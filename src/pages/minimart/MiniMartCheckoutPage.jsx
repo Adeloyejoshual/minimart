4aa8c0de116795a5bbcc2acf5664e70b65399b49
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 00:56:55 +0100

    Create src/pages/minimart/MiniMartCheckoutPage.jsx

---
+++ src/pages/minimart/MiniMartCheckoutPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MiniMartCheckoutPage() {
+  return (
+    <div>
+      <h1>MiniMart Checkout</h1>
+      <p>Checkout page content.</p>
+    </div>
+  );
+}
