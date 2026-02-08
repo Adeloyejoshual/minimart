84edc44e5591efa6b04a00d08ed21cb10c1986df
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 00:56:26 +0100

    Create src/pages/minimart/MiniMartCartPage.jsx

---
+++ src/pages/minimart/MiniMartCartPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MiniMartCartPage() {
+  return (
+    <div>
+      <h1>MiniMart Cart</h1>
+      <p>Your cart is empty.</p>
+    </div>
+  );
+}
