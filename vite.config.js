db1b8b432c7c8e56721293ffeae5d76607e59d80
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 01:34:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist", // matches server static path
+    emptyOutDir: true,
+  },
 });
