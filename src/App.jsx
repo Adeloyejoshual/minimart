f8e274f7419edba215d72705ec70c8cfabdc1d64
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 01:34:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Core Pages
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 
-// -------------------- Marketplace Pages --------------------
+// Marketplace Pages
 import MarketplaceListingDetailsPage from "./pages/marketplace/MarketplaceListingDetailsPage.jsx";
 import MarketplaceAddProductPage from "./pages/marketplace/MarketplaceAddProductPage.jsx";
 import MarketplaceChatPage from "./pages/marketplace/MarketplaceChatPage.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      {/* Core pages */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Core */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
 
-      {/* Marketplace pages */}
-      <Route path="/marketplace/listing/:id" element={<MarketplaceListingDetailsPage />} />
-      <Route path="/marketplace/add-product" element={<MarketplaceAddProductPage />} />
-      <Route path="/marketplace/chat" element={<MarketplaceChatPage />} />
-    </Routes>
+        {/* Marketplace */}
+        <Route path="/marketplace/listing/:id" element={<MarketplaceListingDetailsPage />} />
+        <Route path="/marketplace/add-product" element={<MarketplaceAddProductPage />} />
+        <Route path="/marketplace/chat" element={<MarketplaceChatPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
