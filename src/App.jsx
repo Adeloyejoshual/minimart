d775d92963ece94d83f8ce4e8c4fbf253aaa199d
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 01:26:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 
+// -------------------- Marketplace Pages --------------------
+import MarketplaceListingDetailsPage from "./pages/marketplace/MarketplaceListingDetailsPage.jsx";
+import MarketplaceAddProductPage from "./pages/marketplace/MarketplaceAddProductPage.jsx";
+import MarketplaceChatPage from "./pages/marketplace/MarketplaceChatPage.jsx";
+
 export default function App() {
   return (
     <Routes>
+      {/* Core pages */}
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+
+      {/* Marketplace pages */}
+      <Route path="/marketplace/listing/:id" element={<MarketplaceListingDetailsPage />} />
+      <Route path="/marketplace/add-product" element={<MarketplaceAddProductPage />} />
+      <Route path="/marketplace/chat" element={<MarketplaceChatPage />} />
     </Routes>
   );
 }
