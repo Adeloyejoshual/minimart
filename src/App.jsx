e825615b0b65af12e7e41c2afa5ec9571d60b861
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-09 10:22:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-// Core Pages
+// Pages
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 
-// Marketplace Pages
-import AddProduct from "./pages/Marketplace/AddProduct.jsx";
-import ListingDetails from "./pages/Marketplace/ListingDetails.jsx";
-import Chat from "./pages/Marketplace/Chat.jsx";
+// MiniMart
+import AddMiniMartProduct from "./pages/MiniMart/AddProduct.jsx";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Core */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-        {/* Marketplace */}
-        <Route path="/marketplace/add-product" element={<AddProduct />} />
-        <Route path="/marketplace/listing/:id" element={<ListingDetails />} />
-        <Route path="/marketplace/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+      {/* MiniMart */}
+      <Route path="/minimart/add-product" element={<AddMiniMartProduct />} />
+    </Routes>
   );
 }
+
+export default App;
