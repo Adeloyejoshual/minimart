d72e60b556eac7abf7a8687064c2adfcc75e20a5
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-12 00:56:48 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HomePage from "./pages/HomePage.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<HomePage />);
+<Auth0Provider
+  domain={import.meta.env.VITE_AUTH0_DOMAIN}
+  clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+  authorizationParams={{ redirect_uri: window.location.origin }}
+>
+  <App />
+</Auth0Provider>
