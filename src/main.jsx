35e8484e03fdd229179c6e8a028745a47adf687e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-12 11:55:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-<Auth0Provider
-  domain={import.meta.env.VITE_AUTH0_DOMAIN}
-  clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-  authorizationParams={{ redirect_uri: window.location.origin }}
->
-  <App />
-</Auth0Provider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
