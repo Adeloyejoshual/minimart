43f5e8c13a08fc7e98bb6c4ccbb5d320baeb705d
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-14 13:53:48 +0100

    Create NotFoundPage.jsx

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/NotFoundPage.jsx
+import { Link } from "react-router-dom";
+
+export default function NotFoundPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link to="/">Go Home</Link>
+    </div>
+  );
+}
