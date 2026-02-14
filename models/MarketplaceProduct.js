65e7e3aadc746b65eaa18e21ec96e8ad47b7ed68
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-14 17:03:36 +0100

    Update MarketplaceProduct.js

--- models/MarketplaceProduct.js
+++ models/MarketplaceProduct.js
@@ -7,56 +7,19 @@ const marketplaceProductSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-
-    category: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    subcategory: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
     description: {
       type: String,
-      required: true,
       trim: true,
     },
-
     price: {
       type: Number,
       required: true,
     },
-
-    country: {
-      type: String,
-      default: "Nigeria",
-    },
-
-    state: {
-      type: String,
-      required: true,
-    },
-
-    city: {
-      type: String,
-      required: true,
-    },
-
     image: {
-      type: String, // store image path or URL
-      required: true,
+      type: String, // Cloudinary URL
     },
   },
   { timestamps: true }
 );
 
-const MarketplaceProduct = mongoose.model(
-  "MarketplaceProduct",
-  marketplaceProductSchema
-);
-
-export default MarketplaceProduct;
+export default mongoose.model("MarketplaceProduct", marketplaceProductSchema);
