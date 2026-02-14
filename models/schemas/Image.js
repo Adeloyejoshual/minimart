0be7cbbd500a510bd82315764e1bfe4f55e87c24
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-14 12:23:51 +0100

    Update Image.js

--- models/schemas/Image.js
+++ models/schemas/Image.js
@@ -1,11 +1,11 @@
+// models/schemas/Image.js
 import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 const ImageSchema = new Schema({
   url: { type: String, required: true },
-  alt: { type: String },
-  order: { type: Number, default: 0 },
+  alt: { type: String, default: "" },
 });
 
 export default ImageSchema;
