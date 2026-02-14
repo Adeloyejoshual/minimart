ff3d7ad564ff7bc81257ccbcb701af6082131024
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-14 11:47:34 +0100

    Create src/config/promotions.js

---
+++ src/config/promotions.js
@@ -0,0 +1,5 @@
+export const promotionPlans = [
+  { id: 1, label: "Free Listing", type: "free", price: 0, days: 7 },
+  { id: 2, label: "Featured", type: "paid", price: 1000, days: 7 },
+  { id: 3, label: "Flash Sale", type: "paid", price: 2000, days: 3 },
+];
