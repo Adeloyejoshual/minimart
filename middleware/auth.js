ea423ac1f683c4951d313fd873e28198b2c73bd8
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-14 13:35:10 +0100

    Create middleware/auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+// middleware/auth.js
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: "Unauthorized" });
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ auth0Id: decoded.sub });
+    if (!user) return res.status(401).json({ error: "Unauthorized" });
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
