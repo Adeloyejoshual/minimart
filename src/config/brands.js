dda1dbcbb7c6aeffc0382d3eb35a73bf50c19004
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-16 02:38:14 +0100

    Create brands.js

---
+++ src/config/brands.js
@@ -0,0 +1,127 @@
+// src/config/brands.js
+export const brands = {
+  "Phones & Tablets": [
+    "Apple",
+    "Samsung",
+    "Google",
+    "Huawei",
+    "Xiaomi",
+    "Infinix",
+    "Tecno",
+    "OnePlus",
+    "OPPO"
+  ],
+
+  "Vehicles": [
+    "Toyota",
+    "Honda",
+    "BMW",
+    "Mercedes",
+    "Ford",
+    "Nissan",
+    "Kia",
+    "Lexus",
+    "Volkswagen"
+  ],
+
+  "Babies & Kids": [
+    "Fisher-Price",
+    "Lego",
+    "Chicco",
+    "Graco",
+    "BabyBjorn"
+  ],
+
+  "Property": [
+    "Generic"
+  ],
+
+  "Electronics": [
+    "Sony",
+    "LG",
+    "Samsung",
+    "Panasonic",
+    "Philips",
+    "Dell",
+    "HP"
+  ],
+
+  "Beauty & Personal Care": [
+    "L'Oreal",
+    "Nivea",
+    "Dove",
+    "Maybelline",
+    "P&G"
+  ],
+
+  "Home, Furniture & Appliances": [
+    "IKEA",
+    "Philips",
+    "Samsung",
+    "LG",
+    "Bosch"
+  ],
+
+  "Fashion": [
+    "Nike",
+    "Adidas",
+    "Gucci",
+    "Zara",
+    "H&M",
+    "Louis Vuitton"
+  ],
+
+  "Computers & Laptops": [
+    "Apple",
+    "Dell",
+    "HP",
+    "Lenovo",
+    "Asus",
+    "Acer"
+  ],
+
+  "Gaming": [
+    "Sony",
+    "Microsoft",
+    "Nintendo",
+    "Razer"
+  ],
+
+  "Vehicles Parts & Accessories": [
+    "Bosch",
+    "Denso",
+    "NGK",
+    "Valeo"
+  ],
+
+  "Musical Instruments": [
+    "Yamaha",
+    "Roland",
+    "Fender",
+    "Gibson"
+  ],
+
+  "Toys & Games": [
+    "LEGO",
+    "Mattel",
+    "Hasbro"
+  ],
+
+  "Pets": [
+    "Generic"
+  ],
+
+  "Jobs": [
+    "Generic"
+  ],
+
+  "Services": [
+    "Generic"
+  ],
+
+  "Repair & Construction": [
+    "Bosch",
+    "Makita",
+    "DeWalt"
+  ]
+};
