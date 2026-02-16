e2916d079d959c78e84050d1bd56447fe22d20fa
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-16 01:01:18 +0100

    Update marketplace.js

--- src/helpers/marketplace.js
+++ src/helpers/marketplace.js
@@ -1,29 +1,32 @@
-// src/helpers/marketplace.js
-import axios from "axios";
+import api from "../utils/api";
 
+// GET ALL
 export const getMarketplaceProducts = async (filters = {}) => {
   const params = new URLSearchParams(filters).toString();
-  const res = await axios.get(`/api/marketplace${params ? `?${params}` : ""}`);
+  const res = await api.get(`/api/marketplace${params ? `?${params}` : ""}`);
   return res.data;
 };
 
+// GET ONE
 export const getMarketplaceProductById = async (id) => {
-  const res = await axios.get(`/api/marketplace/${id}`);
+  const res = await api.get(`/api/marketplace/${id}`);
   return res.data;
 };
 
-export const addMarketplaceProduct = async (formData, token) => {
-  const res = await axios.post("/api/marketplace", formData, {
+// ADD (JSON only, no multipart)
+export const addMarketplaceProduct = async (data, token) => {
+  const res = await api.post("/api/marketplace", data, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "Content-Type": "multipart/form-data",
+      "Content-Type": "application/json",
     },
   });
   return res.data;
 };
 
+// DELETE
 export const deleteMarketplaceProduct = async (id, token) => {
-  const res = await axios.delete(`/api/marketplace/${id}`, {
+  const res = await api.delete(`/api/marketplace/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
