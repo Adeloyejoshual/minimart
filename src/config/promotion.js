81bde252f1a860386191200b321dfd4e398561c5
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-16 02:19:21 +0100

    Create promotion.js

---
+++ src/config/promotion.js
@@ -0,0 +1,5 @@
+export const promotionPlans = [
+  { name: "TOP Promo", duration: "7 days", price: 3999 },
+  { name: "Boost Premium", duration: "1 month", price: 31999 },
+  { name: "Trial Free", duration: "14 days", price: 0 },
+];
