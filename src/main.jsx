70476c992e23f5de4ad156fb030583a81e4273b2
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-20 18:21:29 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
-import "./index.css";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
-      useRefreshTokens={true}       // keeps user logged in
-      cacheLocation="localstorage"   // persists session across refresh
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{ redirect_uri: redirectUri }}
+    useRefreshTokens={true}
+    cacheLocation="localstorage"
+  >
+    <App />
+  </Auth0Provider>
 );
