c4bab2a405829b08a05854ca9e201c89d32de380
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-20 23:32:23 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 // main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Auth0ProviderWithRedirect from './components/Auth0ProviderWithRedirect';
+import AppRoutes from './routes/AppRoutes';
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithRedirect>
+        <AppRoutes />
+      </Auth0ProviderWithRedirect>
+    </BrowserRouter>
+  </React.StrictMode>
+);
