717fb030cf7633fc43431345d89d8c89631c659b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-20 17:49:51 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function LoginPage() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isLoading } = useAuth0();
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <div style={{ padding: 20 }}>
