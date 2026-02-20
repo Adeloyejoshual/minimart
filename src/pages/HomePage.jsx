7d407490ffb3c40f7d778a77e22e8438a9216c37
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-20 17:49:27 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function HomePage() {
-  const { isAuthenticated, isLoading, user, logout } = useAuth0();
+  const { isAuthenticated, user, logout, isLoading } = useAuth0();
 
-  if (isLoading) return <p>Loading authentication...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <div style={{ padding: 20 }}>
@@ -12,7 +12,7 @@ export default function HomePage() {
 
       {isAuthenticated ? (
         <div>
-          <p>Hello, {user.name || user.email}</p>
+          <p>Hello, {user?.name || user?.email}</p>
           <button onClick={() => logout({ returnTo: window.location.origin })}>
             Logout
           </button>
