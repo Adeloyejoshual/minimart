09e6b5b2942b8c5ff69a51d0e2b0ca5b9beab90c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-20 15:55:23 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,19 @@
-import { useEffect } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function LoginPage() {
   const { loginWithRedirect } = useAuth0();
 
-  useEffect(() => {
-    loginWithRedirect({ screen_hint: "login" });
-  }, []);
-
-  return <p>Redirecting to login...</p>;
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Login</h2>
+      <button
+        onClick={() =>
+          loginWithRedirect({ authorizationParams: { prompt: "login" } })
+        }
+      >
+        Log In
+      </button>
+    </div>
+  );
 }
