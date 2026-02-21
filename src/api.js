60f569a8607a17ec4ba77c5a292c63e796eb948e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-21 03:17:14 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,20 +1,8 @@
-const BASE_URL = "https://my-backend.onrender.com";
+// src/api.js
+import axios from "axios";
 
-export const getDBTime = async () => {
-  const res = await fetch(`${BASE_URL}/test-db`);
-  return res.json();
-};
+const api = axios.create({
+  baseURL: window.location.origin,
+});
 
-export const getProducts = async () => {
-  const res = await fetch(`${BASE_URL}/products`);
-  return res.json();
-};
-
-export const addProduct = async (name, price) => {
-  const res = await fetch(`${BASE_URL}/products`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, price }),
-  });
-  return res.json();
-};
+export default api; // default export
