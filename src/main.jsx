d2b6383cf11f7156a66dc36c30e485e6bf2cd767
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-21 04:53:05 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,16 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-      audience: audience,
-    }}
-  >
-    <App />
-  </Auth0Provider>
-);
+<Auth0Provider
+domain={domain}
+clientId={clientId}
+authorizationParams={{ redirect_uri: window.location.origin }}
+
+> 
+
+<App />
+
+  </Auth0Provider>  
+);  
