44dbdfa74fe67e1d381f9d9c8305767a94ec5fc1
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-22 02:26:18 +0100

    Update ram.js

--- src/config/ram.js
+++ src/config/ram.js
@@ -1,1 +1,5 @@
-export const ramOptions = ["2 GB","4 GB","6 GB","8 GB","12 GB","16 GB"];
+// src/config/ramOptions.js - COMPLETE MOBILE SPECS (2026)
+export const ramOptions = [
+  "1 GB", "1.5 GB", "2 GB", "3 GB", "4 GB", "6 GB", "8 GB", 
+  "12 GB", "16 GB", "18 GB", "24 GB"
+];
