7158b9930b5e47b560a3137bbabb61dcc23ec44f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-22 02:27:26 +0100

    Update storage.js

--- src/config/storage.js
+++ src/config/storage.js
@@ -1,1 +1,5 @@
-export const storageOptions = ["32 GB","64 GB","128 GB","256 GB","512 GB","1 TB"];
+// src/config/storageOptions.js - COMPLETE 2026 SPECS
+export const storageOptions = [
+  "16 GB", "32 GB", "64 GB", "128 GB", "256 GB", "512 GB", 
+  "1 TB", "2 TB"
+];
