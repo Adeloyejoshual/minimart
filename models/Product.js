734b56678f7cf7c1a34185d97838eaac7d1588ec
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-25 22:48:51 +0100

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,35 +2,12 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    default: 'general'
-  },
-  image: {
-    type: String,
-    default: ''
-  },
-  stock: {
-    type: Number,
-    default: 0,
-    min: 0
-  }
-}, {
-  timestamps: true
-});
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: String,
+  category: { type: String, default: 'general' },
+  image: String,
+  stock: { type: Number, default: 0 }
+}, { timestamps: true });
 
 export const Product = mongoose.model('Product', productSchema);
