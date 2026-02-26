6e694a18a3570135ff742152fbce285dfe839d7d
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-26 15:12:20 +0100

    Delete engine.js

--- src/config/engine.js
+++
@@ -1,6 +0,0 @@
-// src/config/engines.js - COMPLETE ENGINE SIZES
-export const engines = [
-  "0.8 L", "1.0 L", "1.2 L", "1.4 L", "1.5 L", "1.6 L", "1.8 L", "2.0 L", 
-  "2.2 L", "2.4 L", "2.5 L", "2.7 L", "3.0 L", "3.2 L", "3.5 L", "4.0 L", 
-  "4.4 L", "5.0 L", "5.7 L", "6.2 L", "6.6 L", "V6", "V8", "V10", "V12"
-];
