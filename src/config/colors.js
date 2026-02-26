25d7567dc771267517e9c5993fa2ea197a432a86
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-26 15:19:54 +0100

    Create colors.js

---
+++ src/config/colors.js
@@ -0,0 +1,8 @@
+// src/config/colors.js - EXPANDED COLORS
+export const colors = [
+  "Black", "White", "Blue", "Red", "Green", "Yellow", "Purple", "Silver", "Gold",
+  "Gray", "Grey", "Pink", "Orange", "Brown", "Beige", "Navy", "Teal", "Cyan",
+  "Magenta", "Maroon", "Olive", "Lavender", "Rose Gold", "Champagne", "Bronze",
+  "Copper", "Coral", "Indigo", "Mint", "Peach", "Plum", "Titanium", "Space Gray",
+  "Midnight", "Starlight", "Natural Titanium", "Jet Black", "Matte Black"
+];
