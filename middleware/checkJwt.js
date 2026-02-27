686921a1543e244a5bdad6827a1f177fe1c8c8ad
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-27 18:40:53 +0100

    Update checkJwt.js

--- middleware/checkJwt.js
+++ middleware/checkJwt.js
@@ -2,6 +2,10 @@
 
 import { auth } from "express-oauth2-jwt-bearer";
 
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw new Error("AUTH0_DOMAIN and AUTH0_AUDIENCE must be set");
+}
+
 export const checkJwt = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
