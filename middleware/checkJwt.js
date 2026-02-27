ddb738ae1c609825f228b7b945c4edfd4ede97dd
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-27 18:31:13 +0100

    Create middleware/checkJwt.js

---
+++ middleware/checkJwt.js
@@ -0,0 +1,9 @@
+// middleware/checkJwt.js
+
+import { auth } from "express-oauth2-jwt-bearer";
+
+export const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
+  tokenSigningAlg: "RS256",
+});
