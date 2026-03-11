c224901d81ad3098392138f15fc41680f9d9bdce
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-11 23:41:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// Pages
-import MiniMartHome from "./pages/MiniMart/Home";
+import Home from "./pages/Home";
 import AddProduct from "./pages/AddProduct";
 import ProductDetail from "./pages/ProductDetail";
 import Search from "./pages/Search";
 import SellerProfile from "./pages/SellerProfile";
+
+import MiniMartBottomNav from "./components/MiniMartBottomNav";
 
 function App() {
   return (
@@ -18,15 +20,19 @@ function App() {
         audience: process.env.REACT_APP_AUTH0_AUDIENCE,
       }}
     >
-      <Router>
-        <Routes>
-          <Route path="/" element={<MiniMartHome />} />
-          <Route path="/minimart/add" element={<AddProduct />} />
-          <Route path="/minimart/:id" element={<ProductDetail />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/seller/:id" element={<SellerProfile />} />
-        </Routes>
-      </Router>
+      <BrowserRouter>
+        <div style={{ paddingBottom: "80px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/minimart" element={<Home />} />
+            <Route path="/minimart/add" element={<AddProduct />} />
+            <Route path="/minimart/:id" element={<ProductDetail />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/seller/:id" element={<SellerProfile />} />
+          </Routes>
+          <MiniMartBottomNav />
+        </div>
+      </BrowserRouter>
     </Auth0Provider>
   );
 }
