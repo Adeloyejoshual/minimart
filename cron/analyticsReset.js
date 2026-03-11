c18556acc50f3372dba5b5d5dd5f8cf33e92b027
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-11 05:09:31 +0100

    Delete analyticsReset.js

--- cron/analyticsReset.js
+++
@@ -1,7 +0,0 @@
-// cron/analyticsReset.js - Daily views reset
-import cron from 'node-cron';
-import MarketplaceProduct from '../models/MarketplaceProduct.js';
-
-cron.schedule('0 0 * * *', async () => {
-  await MarketplaceProduct.updateMany({ active: true }, { views_today: 0 });
-});
