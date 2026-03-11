78abccc1e8471f2718ab20f2afbfcdede7869f23
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-11 23:18:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MiniMartHome from "./pages/MiniMart/Home.jsx";
-import AddMiniMartProduct from "./pages/MiniMart/AddProduct.jsx";
-import ProductDetail from "./pages/MiniMart/ProductDetail.jsx";
 
-export default function App() {
+// Layout
+import Layout from "./layout/Layout";
+
+// Pages
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import AddProduct from "./pages/AddProduct";
+import Search from "./pages/Search";
+import SellerProfile from "./pages/SellerProfile";
+import NotFound from "./pages/NotFound";
+
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MiniMartHome />} />
-        <Route path="/minimart/add" element={<AddMiniMartProduct />} />
-        <Route path="/minimart/:id" element={<ProductDetail />} />
-      </Routes>
+      <Layout>
+        <Routes>
+
+          {/* Homepage */}
+          <Route path="/" element={<Home />} />
+
+          {/* Product page */}
+          <Route path="/product/:id" element={<ProductDetails />} />
+
+          {/* Add product */}
+          <Route path="/sell" element={<AddProduct />} />
+
+          {/* Search */}
+          <Route path="/search" element={<Search />} />
+
+          {/* Seller profile */}
+          <Route path="/seller/:id" element={<SellerProfile />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+
+        </Routes>
+      </Layout>
     </Router>
   );
 }
+
+export default App;
