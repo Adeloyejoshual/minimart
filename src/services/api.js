9c45f56e2a3fd63e07937cd2a5810372c3230941
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-12 18:55:11 +0100

    Delete api.js

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "/api",
-});
-
-export const getProducts = () => API.get("/marketplace");
-export const getProduct = (id) => API.get(`/marketplace/${id}`);
-export const addProduct = (data) => API.post("/marketplace", data);
