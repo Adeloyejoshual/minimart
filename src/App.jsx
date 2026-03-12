108ef890d8b3b59cb483d63597d4a312d570ed8c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-12 22:56:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 // src/App.jsx
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
+import AddProduct from "./pages/AddProduct";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/add-product" element={<AddProduct />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
