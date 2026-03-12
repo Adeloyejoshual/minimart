2dbe6830c5a1511adeea7df25eba46a765f7c8d5
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-12 22:14:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
 import Homepage from "./pages/Homepage";
-import AddProduct from "./pages/AddProduct";
-import PrivateRoute from "./components/PrivateRoute"; // ensures AddProduct requires login
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Homepage */}
-        <Route path="/" element={<Homepage />} />
-
-        {/* Protected Add Product page */}
-        <Route
-          path="/add-product"
-          element={
-            <PrivateRoute>
-              <AddProduct />
-            </PrivateRoute>
-          }
-        />
-
-        {/* fallback route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-  );
+  return <Homepage />;
 }
 
 export default App;
