56d2792fa12ffbbe7cde32eb2b6842061db1f710
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-12 21:21:54 +0100

    Update sendMail.js

--- email/sendMail.js
+++ email/sendMail.js
@@ -1,4 +1,3 @@
-// email/sendMail.js
 import nodemailer from "nodemailer";
 import { google } from "googleapis";
 import dotenv from "dotenv";
@@ -9,22 +8,26 @@ const {
   GMAIL_CLIENT_ID,
   GMAIL_CLIENT_SECRET,
   GMAIL_REFRESH_TOKEN,
-  GMAIL_USER_EMAIL,
+  GMAIL_USER_EMAIL
 } = process.env;
 
-const oAuth2Client = new google.auth.OAuth2(
+const OAuth2 = google.auth.OAuth2;
+
+const oAuth2Client = new OAuth2(
   GMAIL_CLIENT_ID,
   GMAIL_CLIENT_SECRET,
   "https://developers.google.com/oauthplayground"
 );
 
-oAuth2Client.setCredentials({ refresh_token: GMAIL_REFRESH_TOKEN });
+oAuth2Client.setCredentials({
+  refresh_token: GMAIL_REFRESH_TOKEN
+});
 
 export const sendMail = async (to, subject, html) => {
   try {
     const accessToken = await oAuth2Client.getAccessToken();
 
-    const transport = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         type: "OAuth2",
@@ -32,21 +35,21 @@ export const sendMail = async (to, subject, html) => {
         clientId: GMAIL_CLIENT_ID,
         clientSecret: GMAIL_CLIENT_SECRET,
         refreshToken: GMAIL_REFRESH_TOKEN,
-        accessToken: accessToken?.token,
-      },
+        accessToken: accessToken
+      }
     });
 
     const mailOptions = {
       from: `"MiniMart" <${GMAIL_USER_EMAIL}>`,
       to,
       subject,
-      html,
+      html
     };
 
-    const result = await transport.sendMail(mailOptions);
-    return result;
+    return await transporter.sendMail(mailOptions);
+
   } catch (error) {
-    console.error("Failed to send email:", error);
+    console.error("Email sending failed:", error);
     throw new Error("Email sending failed");
   }
 };
