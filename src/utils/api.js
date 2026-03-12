0d754677fe7fc9b15b947e98fdd37d9bff7f4004
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-12 18:55:34 +0100

    Delete api.js

--- src/utils/api.js
+++
@@ -1,9 +0,0 @@
-// src/utils/api.js
-import axios from "axios";
-
-// Use environment variable for API URL, fallback to relative path
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api/marketplace",
-});
-
-export default api;
