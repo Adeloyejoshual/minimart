20e5f97cc78ad94eb89f0a7883a532fc857d1e16
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-12 23:14:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import AddProduct from "./pages/AddProduct";
 
 function App() {
-  return (
-    <Router>
-      <header style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
-        <Link to="/" style={{ marginRight: "10px" }}>Marketplace</Link>
-        <Link to="/add-product">Add Product</Link>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/add-product" element={<AddProduct />} />
-      </Routes>
-    </Router>
-  );
+  return <Homepage />;
 }
 
 export default App;
