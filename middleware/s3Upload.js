666953abb148267388a3bd877b4cea52050fa23d
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-13 21:40:40 +0100

    Update s3Upload.js

--- middleware/s3Upload.js
+++ middleware/s3Upload.js
@@ -14,7 +14,7 @@ const s3 = new aws.S3({
 export const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.S3_BUCKET,
+    bucket: process.env.S3_BUCKET, // must exist
     acl: "public-read",
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
