ee677b001d7587efb844b4cc0180c5df97fc0529
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-13 19:22:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import AddProduct from "./pages/AddProduct";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/minimart/add" element={<AddProduct />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  );
+  return <Homepage />;
 }
