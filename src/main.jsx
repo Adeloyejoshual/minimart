3364bfa17d3c8475bdbf89943ec0ec183bdacc08
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-13 19:50:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
+// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
