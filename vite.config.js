404c19e5c95163c3ace40b4dd17e80f5bbcdef49
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-15 00:34:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      external: [],
     },
   },
 });
