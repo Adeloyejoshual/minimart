398227b9da7f707ba3050c143803bdc071fc1da2
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-14 23:24:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import AddProduct from "./pages/AddProduct";
+import ProductDetail from "./pages/ProductDetail";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Homepage />} />
+
+        <Route path="/product/:id" element={<ProductDetail />} />
+
         <Route path="/minimart/add" element={<AddProduct />} />
+
       </Routes>
     </Router>
   );
