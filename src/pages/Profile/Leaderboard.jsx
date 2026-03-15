a07bf73fba7da80709419af0bab7ca4e4bcec5b9
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-15 21:23:14 +0100

    Update Leaderboard.jsx

--- src/pages/Profile/Leaderboard.jsx
+++ src/pages/Profile/Leaderboard.jsx
@@ -1,7 +1,7 @@
 // Page/Profile/Leaderboard.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import '../style/Profile.css';
+import '../../style/Profile.css';
 
 const Leaderboard = () => {
   const [leaders, setLeaders] = useState([]);
