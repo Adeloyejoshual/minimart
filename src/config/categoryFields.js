33cdeb6bd32c8e294ea66789ad3cd9c791ce4b9f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-18 22:56:02 +0100

    Update categoryFields.js

--- src/config/categoryFields.js
+++ src/config/categoryFields.js
@@ -1,6 +1,6 @@
-// src/config/categoryFields.js - NO EXCHANGE FIELDS
+// src/config/categoryFields.js
 export const categoryFields = {
-  "Phones & Tablets": [
+  "102055d1-180a-4b8f-a39b-3b20a4838e90": [ // Phones & Tablets
     "brand",
     "model",
     "condition",
@@ -12,7 +12,7 @@ export const categoryFields = {
     "features"
   ],
 
-  "Vehicles": [
+  "b2345835-2bf3-4749-a1e9-760e8159ecc6": [ // Vehicles
     "brand",
     "model",
     "condition",
@@ -26,14 +26,14 @@ export const categoryFields = {
     "features"
   ],
 
-  "Babies & Kids": [
+  "754e63f4-7e20-483c-a9c2-6782e615bd2d": [ // Babies & Kids
     "age_range",
     "condition",
     "used_detail",
     "features"
   ],
 
-  "Property": [
+  "c96bba5b-a9f8-43ed-8dbb-3326f34e07c0": [ // Property
     "bedrooms",
     "bathrooms",
     "size",
@@ -41,7 +41,7 @@ export const categoryFields = {
     "features"
   ],
 
-  "Electronics": [
+  "bba9b3e7-4118-42c4-9ea9-4aa2afd445dc": [ // Electronics
     "brand",
     "model",
     "condition",
@@ -49,73 +49,73 @@ export const categoryFields = {
     "features"
   ],
 
-  "Beauty & Personal Care": [
+  "4aba6a69-2b1c-4b19-9ca0-3b2630ef6fdb": [ // Beauty & Personal Care
     "brand",
     "features"
   ],
 
-  "Home, Furniture & Appliances": [
+  "4bb82894-f6aa-478a-8541-da3305d5a293": [ // Home, Furniture & Appliances
     "brand",
     "features",
     "condition",
     "used_detail"
   ],
 
-  "Commercial Equipment & Tools": [
+  "39dc4492-0754-4826-816b-bc32f31081d0": [ // Commercial Equipment & Tools
     "brand",
     "features",
     "condition",
     "used_detail"
   ],
 
-  "Fashion": [
+  "8ba64fb7-33a6-415e-a895-38d778a49075": [ // Fashion
     "brand",
     "features",
     "size",
     "color"
   ],
 
-  "Food, Agriculture & Farming": [
+  "cf185f2a-d291-40cc-8694-67291f1a6a26": [ // Food, Agriculture & Farming
     "features",
     "condition"
   ],
 
-  "Leisure & Activities": [
+  "4d13f1aa-bd53-49a1-9e86-cf33ece1b254": [ // Leisure & Activities
     "features",
     "condition"
   ],
 
-  "Pets": [
+  "e70d46b2-9450-42ee-a938-4235c319b8b3": [ // Pets
     "age_range",
     "breed",
     "features",
     "condition"
   ],
 
-  "Jobs": [
+  "3079d791-8695-47ef-aaa1-78b9eabb32fe": [ // Jobs
     "experience_level",
     "skills",
     "education"
   ],
 
-  "Repair & Construction": [
+  "46f8dcab-69d0-4fa0-aead-f9ab6c64c139": [ // Repair & Construction
     "brand",
     "features",
     "condition",
     "used_detail"
   ],
 
-  "Seeking Work CVs": [
+  "d6b767d7-1f3b-46cc-9e67-00b699e4ec04": [ // Seeking Work CVs
     "experience_level",
     "skills",
     "education"
   ],
 
-  "Services": [
+  "20371324-5130-4952-91ed-29cf67c93f72": [ // Services
     "features"
   ],
 
-  "Vehicles Parts & Accessories": [
+  "cb32087f-c235-466e-9e75-6fbee393903b": [ // Vehicles Parts & Accessories
     "brand",
     "model",
     "condition",
@@ -123,13 +123,13 @@ export const categoryFields = {
     "features"
   ],
 
-  "Books & Stationery": [
+  "947ce100-d961-4455-bfbf-c1d33537f11b": [ // Books & Stationery
     "condition",
     "used_detail",
     "features"
   ],
 
-  "Musical Instruments": [
+  "e6d02486-ce55-4718-a096-6af8001d4a2c": [ // Musical Instruments
     "brand",
     "model",
     "condition",
@@ -137,14 +137,14 @@ export const categoryFields = {
     "features"
   ],
 
-  "Sports & Outdoors": [
+  "3c93ad90-2b69-4072-b2cb-748384f44d3f": [ // Sports & Outdoors
     "brand",
     "condition",
     "used_detail",
     "features"
   ],
 
-  "Computers & Laptops": [
+  "fc1acba9-a5ca-4a82-8305-81586ecb75e1": [ // Computers & Laptops
     "brand",
     "model",
     "condition",
@@ -154,7 +154,7 @@ export const categoryFields = {
     "features"
   ],
 
-  "Gaming": [
+  "b236303d-3ccf-4169-8321-81243d796481": [ // Gaming
     "brand",
     "model",
     "condition",
@@ -162,19 +162,19 @@ export const categoryFields = {
     "features"
   ],
 
-  "Health & Fitness": [
+  "6609d41f-7fd5-469d-8155-9a7c0a7d05f3": [ // Health & Fitness
     "features",
     "condition",
     "used_detail"
   ],
 
-  "Art & Collectibles": [
+  "85d13ecd-a84a-4c39-8358-db890206e280": [ // Art & Collectibles
     "features",
     "condition",
     "used_detail"
   ],
 
-  "Toys & Games": [
+  "d30edb05-1f94-41e6-9400-6f8d8252a29b": [ // Toys & Games
     "brand",
     "age_range",
     "condition",
