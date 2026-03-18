35636c372a98f8bc59ba19e2c53d3e8c9141617c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-18 22:08:23 +0100

    Delete sim.js

--- src/config/sim.js
+++
@@ -1,8 +0,0 @@
-// src/config/sim.js - COMPLETE SIM OPTIONS (2026 Nigeria)
-export const sims = [
-  "Single SIM", 
-  "Dual SIM", 
-  "Dual SIM + eSIM", 
-  "eSIM Only", 
-  "Hybrid SIM (SIM/microSD)"
-];
