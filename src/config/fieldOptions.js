32c430acd8c7e3b51560a3399168a525ff8fbcd5
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-24 13:46:26 +0100

    Update fieldOptions.js

--- src/config/fieldOptions.js
+++ src/config/fieldOptions.js
@@ -1,22 +1,10 @@
 // src/config/fieldOptions.js
 export const fieldOptions = {
-  color: ["Black", "White", "Red", "Blue", "Green", "Silver", "Gold"],
-
-  screen_size: ["4.0\"", "5.0\"", "5.5\"", "6.0\"", "6.5\"", "6.7\"", "7.0\""],
-
-  mileage: ["0-10k km", "10-30k km", "30-60k km", "60-100k km", "100k+ km"],
-
-  transmission: ["Manual", "Automatic", "Semi-automatic"],
-
+  
+size: ["XS", "S", "M", "L", "XL", "XXL"],
   age_range: ["0-6m", "6-12m", "1-3y", "3-6y", "6-12y"],
-
   bedrooms: ["1", "2", "3", "4", "5+"],
-
   bathrooms: ["1", "2", "3", "4+"],
-
-  furnished: ["Furnished", "Semi-furnished", "Unfurnished"],
-
   experience_level: ["Entry", "Mid", "Senior", "Expert"],
-
-  skills: ["HTML/CSS", "React", "Node.js", "Python", "Sales", "Marketing"],
+  skills: ["HTML/CSS", "React", "Node.js", "Python", "Sales", "Marketing"]
 };
