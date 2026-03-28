fee1ad928da1dc0128d92a2ec3a4c3eeb89e5e75
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-28 02:35:34 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import { ProductCacheProvider } from "./context/ProductCacheContext";
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ProductCacheProvider>
-      <App />
-    </ProductCacheProvider>
+    <App />
   </React.StrictMode>
 );
