e79c2e724918cb9fc66dcbd099aa1324c5a6c266
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-28 03:40:56 +0100

    Update conditions.js

--- src/config/conditions.js
+++ src/config/conditions.js
@@ -1,14 +1,2 @@
-/* ================= PRODUCT CONDITIONS ================= */
-export const conditions = [
-  { value: "brand_new", label: "Brand New" },
-  { value: "used", label: "Used" },
-  { value: "refurbished", label: "Refurbished" }
-];
-
-/* ================= USED DETAILS (ONLY FOR "used") ================= */
-export const usedDetails = [
-  { value: "like_new", label: "Like New" },
-  { value: "good", label: "Good" },
-  { value: "fair", label: "Fair" },
-  { value: "for_parts", label: "For Parts" }
-];
+export const conditions = ["Brand New","Used","Refurbished"];
+export const usedDetails = ["Like New","Good","Fair","For Parts"];
