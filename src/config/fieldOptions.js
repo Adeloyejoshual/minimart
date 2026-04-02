0f2541c13342527deb0fd954c8b05dc01e1c60b7
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-02 23:07:31 +0100

    Update fieldOptions.js

--- src/config/fieldOptions.js
+++ src/config/fieldOptions.js
@@ -1,10 +1,64 @@
-// src/config/fieldOptions.js
 export const fieldOptions = {
-  
-size: ["XS", "S", "M", "L", "XL", "XXL"],
-  age_range: ["0-6m", "6-12m", "1-3y", "3-6y", "6-12y"],
-  bedrooms: ["1", "2", "3", "4", "5+"],
-  bathrooms: ["1", "2", "3", "4+"],
-  experience_level: ["Entry", "Mid", "Senior", "Expert"],
-  skills: ["HTML/CSS", "React", "Node.js", "Python", "Sales", "Marketing"]
+  size: ["XS", "S", "M", "L", "XL", "XXL"],
+
+  age_range: [
+    "0-3m",
+    "3-6m",
+    "6-12m",
+    "1-2y",
+    "2-3y",
+    "3-5y",
+    "6-8y",
+    "9-12y",
+    "13-17y"
+  ],
+
+  bedrooms: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10+"
+  ],
+
+  bathrooms: [
+    "1",
+    "1.5",
+    "2",
+    "2.5",
+    "3",
+    "3.5",
+    "4",
+    "5",
+    "6+"
+  ],
+
+  experience_level: [
+    "Entry",
+    "Junior",
+    "Mid",
+    "Senior",
+    "Lead",
+    "Expert"
+  ],
+
+  skills: [
+    "HTML/CSS",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Express.js",
+    "Python",
+    "SQL",
+    "MongoDB",
+    "Sales",
+    "Marketing",
+    "UI/UX Design",
+    "Product Management"
+  ]
 };
