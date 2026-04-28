7421f61c5075b8ca7869c55fc237103cf1d337dc
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-28 20:16:06 +0100

    Update marketplace.js

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -6,20 +6,24 @@ import fs from "fs/promises";
 import dotenv from "dotenv";
 
 import authenticate from "../middleware/auth.js";
-import { brands } from "../src/config/brands.js";
-import { colors } from "../src/config/colors.js";
-import { categoryFields } from "../src/config/categoryFields.js";
-import { conditions, usedDetails } from "../src/config/conditions.js";
-import { featuresByCategory } from "../src/config/featuresByCategory.js";
-import { models } from "../src/config/models.js";
-import { ramOptions } from "../src/config/ramOptions.js";
-import { sims } from "../src/config/sims.js";
-import { storageOptions } from "../src/config/storageOptions.js";
-import { years } from "../src/config/years.js";
-import { engines } from "../src/config/engines.js";
-import { fuelTypes } from "../src/config/fuelTypes.js";
-import { locationsByState } from "../src/config/locationsByState.js";
-import { promotionPlans } from "../src/config/promotions.js";
+
+import {
+  brands,
+  colors,
+  categoryFields,
+  conditions,
+  usedDetails,
+  featuresByCategory,
+  models,
+  ramOptions,
+  sims,
+  storageOptions,
+  years,
+  engines,
+  fuelTypes,
+  locationsByState,
+  promotionPlans,
+} from "../src/config/index.js";
 
 dotenv.config();
 
