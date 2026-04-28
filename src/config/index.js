4b9881246314bed6c14483285627d38bc5a08010
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-28 21:29:59 +0100

    Create src/config/index.js

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+// src/config/index.js
+export { brands } from "./brands.js";
+export { colors } from "./colors.js";
+export { categoryFields } from "./categoryFields.js";
+export { conditions, usedDetails } from "./conditions.js";
+export { featuresByCategory } from "./featuresByCategory.js";
+export { models } from "./models.js";
+export { ramOptions } from "./ramOptions.js";
+export { sims } from "./sims.js";
+export { storageOptions } from "./storageOptions.js";
+export { years } from "./years.js";
+export { engines } from "./engines.js";
+export { fuelTypes } from "./fuelTypes.js";
+export { locationsByState } from "./locationsByState.js";
+export { promotionPlans } from "./promotions.js";
