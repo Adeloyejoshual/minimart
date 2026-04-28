8cbbf95e24b6ee85a321234c7801750d4cc5e992
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-28 21:24:51 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,19 @@
-import pkg from "pg";
-const { Pool } = pkg;
+// config/db.js
+import { Pool } from "pg";
+import { v2 as cloudinary } from "cloudinary";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.COCKROACH_URI,
   ssl: { rejectUnauthorized: false },
 });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default pool;
