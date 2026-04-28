41960bddba54825f4ebe285f47aefc6bd4834567
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-28 21:31:13 +0100

    Create upload.utils.js

---
+++ services/upload.utils.js
@@ -0,0 +1,12 @@
+// utils/multer.js
+import multer from "multer";
+
+const upload = multer({
+  dest: "uploads/",
+  limits: {
+    fileSize: 3 * 1024 * 1024, // 3MB
+    files: 6,
+  },
+});
+
+export { upload };
