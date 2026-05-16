a66ca02ec17f1b493a8ba44c1b091685b70523fa
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-05-16 18:19:41 +0100

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,14 +1,13 @@
-// config/redis.js
 import Redis from "ioredis";
 
-const redisUrl = `rediss://:${process.env.REDIS_PASSWORD}@redis-11056.crce275.eu-west3-1.gcp.cloud.redislabs.com:11056`;
+const redis = new Redis({
+  host: "redis-11056.crce275.eu-west3-1.gcp.cloud.redislabs.com",
+  port: 11056,
+  password: process.env.REDIS_PASSWORD, // IMPORTANT
+  tls: {}, // required for Redis Cloud
+});
 
-export const pub = new Redis(redisUrl);
-export const sub = new Redis(redisUrl);
-export const redis = new Redis(redisUrl);
+redis.on("connect", () => console.log("Redis connected"));
+redis.on("error", (err) => console.error("Redis error", err));
 
-pub.on("connect", () => console.log("Redis pub connected"));
-sub.on("connect", () => console.log("Redis sub connected"));
-
-pub.on("error", (err) => console.error("Redis pub error", err));
-sub.on("error", (err) => console.error("Redis sub error", err));
+export default redis;
