30dfd67e8534f3498bdc3f1d52051ca6395fbdc1
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-05-22 11:17:57 +0100

    Update verification.js

--- routes/verification.js
+++ routes/verification.js
@@ -1,5 +1,5 @@
 import express              from "express";
-import bcrypt               from "bcryptjs";
+import bcrypt               from "bcryptj";
 import crypto               from "crypto";
 import rateLimit            from "express-rate-limit";
 import { pool }             from "../db.js";
