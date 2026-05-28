7ab6ff3fbf7ceb327872a7148482ce8136b55509
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-05-28 22:03:39 +0100

    Create src/constants/termsVersion.js

---
+++ src/constants/termsVersion.js
@@ -0,0 +1,9 @@
+// src/constants/termsVersion.js
+
+/**
+ * Increment version when legal content changes.
+ * Format: YYYY.MM
+ * Any user who accepted a previous version will be
+ * prompted to re-accept automatically.
+ */
+export const TERMS_VERSION = "2025.01";
