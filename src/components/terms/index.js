7fcece96301a6eaa127253af472d9b7567e40600
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-05-28 21:58:30 +0100

    Create src/components/terms/index.js

---
+++ src/components/terms/index.js
@@ -0,0 +1,24 @@
+// src/components/terms/index.js
+
+// ── Layout components ──
+export { default as TermsHeader   } from "./TermsHeader";
+export { default as TermsFooter   } from "./TermsFooter";
+export { default as ProgressBar   } from "./ProgressBar";
+export { default as WarningCard   } from "./WarningCard";
+export { default as TermsSection  } from "./TermsSection";
+
+// ── Content sections ──
+export { default as LegalCompliance      } from "./sections/LegalCompliance";
+export { default as PaymentsSection      } from "./sections/PaymentsSection";
+export { default as ProhibitedItems      } from "./sections/ProhibitedItems";
+export { default as ListingGuidelines    } from "./sections/ListingGuidelines";
+export { default as SafetySection        } from "./sections/SafetySection";
+export { default as PrivacySection       } from "./sections/PrivacySection";
+export { default as FraudSection         } from "./sections/FraudSection";
+export { default as AccountSection       } from "./sections/AccountSection";
+export { default as LiabilitySection     } from "./sections/LiabilitySection";
+export { default as JurisdictionSection  } from "./sections/JurisdictionSection";
+export { default as ElectronicAcceptance } from "./sections/ElectronicAcceptance";
+export { default as TermsChanges         } from "./sections/TermsChanges";
+export { default as ContactSection       } from "./sections/ContactSection";
+export { default as AcceptanceNotice     } from "./sections/AcceptanceNotice";
