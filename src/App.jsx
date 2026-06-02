30ef0dd4ad8746372c4c8211b4b5f6828db4e806
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-06-02 12:21:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ import P2P             from "./pages/P2P";
 import MenuPage        from "./pages/MenuPage";
 
 // ── Pages — Homepage Sub-pages ────────────────────────────────
-import NearbyPage      from "./pages/NearbyPage";
-import DealsPage       from "./pages/DealsPage";
-import NewArrivalsPage from "./pages/NewArrivalsPage";
-import TrendingPage    from "./pages/TrendingPage";
+import NearbyPage      from "./Homepage/pages/NearbyPage";
+import DealsPage       from "./Homepage/pages/DealsPage";
+import NewArrivalsPage from "./Homepage/pages/NewArrivalsPage";
+import TrendingPage    from "./Homepage/TrendingPage";
 
 // ── Pages — Auth ──────────────────────────────────────────────
 import AuthPage        from "./pages/AuthPage";
