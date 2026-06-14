ecebd69d3ddb9501905c8c45d280095d0765da1c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-06-15 00:50:32 +0100

    Create RecommendedProducts.jsx

---
+++ src/components/Cart/RecommendedProducts.jsx
@@ -0,0 +1,11 @@
+// src/components/Cart/RecommendedProducts.jsx
+import React from "react";
+
+export default function RecommendedProducts() {
+  return (
+    <section style={{ marginTop: "2rem" }}>
+      <h2>You may also like</h2>
+      {/* fetch + render recommended products here */}
+    </section>
+  );
+}
