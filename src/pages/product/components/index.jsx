a876788261a98ff7e62c592a7af5b98ae34fbf75
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-07-17 02:36:28 +0100

    Create src/pages/product/components/index.jsx

---
+++ src/pages/product/components/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * src/pages/product/components/index.jsx
+ * Single import point for all product components
+ */
+
+export { default as SectionDot            } from "./SectionDot.jsx";
+export { default as CharCounter           } from "./CharCounter.jsx";
+export { default as PaymentCountdown      } from "./PaymentCountdown.jsx";
+export { default as ExistingImageGrid     } from "./ExistingImageGrid.jsx";
+export { default as ImageGrid             } from "./ImageGrid.jsx";
+export { default as VerificationUpsellModal  } from "./VerificationUpsellModal.jsx";
+export { default as VerificationNudgeBanner  } from "./VerificationNudgeBanner.jsx";
+export * from "./icons/index.jsx";
