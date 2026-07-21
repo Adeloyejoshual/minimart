98630e642fe4bb0a575b7706dece9e614aa936fa
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-07-21 22:39:12 +0100

    Update DealsPage.jsx

--- src/pages/Homepage/DealsPage.jsx
+++ src/pages/Homepage/DealsPage.jsx
@@ -8,15 +8,15 @@ import {
   memo,
 } from "react";
 import { useNavigate } from "react-router-dom";
-import TopNav          from "../components/TopNav";
-import BottomNav       from "../components/BottomNav";
-import Footer          from "../components/Footer";
+import TopNav          from "../../components/TopNav";
+import BottomNav       from "../../components/BottomNav";
+import Footer          from "../../components/Footer";
 import MasonryCard, {
   naira,
   getImageUrl,
   formatCity,
   PinIcon,
-}                      from "../components/MasonryCard";
+}                      from "../../components/MasonryCard";
 import "../../styles/DealsPage.css";
 
 /* ══════════════════════════════════════════════════════════════
