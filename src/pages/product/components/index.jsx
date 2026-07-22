78d35d1c2d88b2de692195a381bb3f9627539ef0
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-07-22 16:48:23 +0100

    Update index.jsx

--- src/pages/product/components/index.jsx
+++ src/pages/product/components/index.jsx
@@ -3,11 +3,12 @@
  * Single import point for all product components
  */
 
-export { default as SectionDot            } from "./SectionDot.jsx";
-export { default as CharCounter           } from "./CharCounter.jsx";
-export { default as PaymentCountdown      } from "./PaymentCountdown.jsx";
-export { default as ExistingImageGrid     } from "./ExistingImageGrid.jsx";
-export { default as ImageGrid             } from "./ImageGrid.jsx";
+export { default as SectionDot               } from "./SectionDot.jsx";
+export { default as CharCounter              } from "./CharCounter.jsx";
+export { default as PaymentCountdown         } from "./PaymentCountdown.jsx";
+export { default as ExistingImageGrid        } from "./ExistingImageGrid.jsx";
+export { default as ImageGrid                } from "./ImageGrid.jsx";
 export { default as VerificationUpsellModal  } from "./VerificationUpsellModal.jsx";
 export { default as VerificationNudgeBanner  } from "./VerificationNudgeBanner.jsx";
+export { default as TermsCheckbox            } from "./TermsCheckbox.jsx";
 export * from "./icons/index.jsx";
